8e7ba06c0e871887c1aec2279cff587ad37cb81b
chenleijiushihao <825040620@qq.com>
2020-05-14 23:39:15 +0800

    add

--- 个人项目/js/index.js
+++ 个人项目/js/index.js
@@ -1,7 +1,16 @@
 // 输入框获取焦点显示搜索框
-let container = document.querySelector('.container'),
-    header = container.querySelector('#head')
+
 
 function search() {
-    
+    let container = document.querySelector('.container'),
+        header = container.querySelector('#head'),
+        searchText = container.querySelector('#search-text'),
+        searchResult = container.querySelector('.search-result');
+    searchText.onfocus = function () {
+        searchResult.style.display = 'block';
+    }
+    searchText.onblur = function () {
+        searchResult.style.display = 'none';
+    }
 }
+search();
